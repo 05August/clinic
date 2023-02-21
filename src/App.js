613f1d7cd0217a780bf2993c7f8e1154e660ee9d
songtoantd18 <songtoantd18@gmail.com>
2023-02-21 21:31:32 +0700

    login,register ui/ux 80%

--- src/App.js
+++ src/App.js
@@ -1,36 +1,49 @@
-import { Empty } from "antd";
-import { Routes, Route, Link } from "react-router-dom";
-import { ROUTE } from "./constants/constants";
-import DefaultLayout from "./layouts/DefaultLayout";
-import Home from "./pages/user/Home/Home";
-import "./styles/style.scss";
-function App() {
+// import { Empty } from "antd";
+// import { Routes, Route, Link } from "react-router-dom";
+// import { ROUTE } from "./constants/constants";
+// import DefaultLayout from "./layouts/DefaultLayout";
+// import Home from "./pages/user/Home/Home";
+// import "./styles/style.scss";
+// function App() {
+//   return (
+//     <div className="App">
+//       <Routes>
+//         <Route
+//           path={ROUTE.NOT_FOUND}
+//           element={
+//             <Link
+//               to={ROUTE.HOME}
+//               style={{
+//                 display: "block",
+//                 margin: "20% auto",
+//                 fontWeight: "bold",
+//                 fontSize: 40,
+//                 width: 475,
+//               }}
+//             >
+//               <Empty />
+//             </Link>
+//           }
+//         />
+//         <Route path={ROUTE.HOME} element={<DefaultLayout />}>
+//           <Route index element={<Home />} />
+//         </Route>
+//       </Routes>
+//     </div>
+//   );
+// }
+
+// export default App;
+import React from "react";
+import Footer from "./components/Footer/Footer";
+import Login from "./components/Login/Login";
+
+const App = () => {
   return (
-    <div className="App">
-      <Routes>
-        <Route
-          path={ROUTE.NOT_FOUND}
-          element={
-            <Link
-              to={ROUTE.HOME}
-              style={{
-                display: "block",
-                margin: "20% auto",
-                fontWeight: "bold",
-                fontSize: 40,
-                width: 475,
-              }}
-            >
-              <Empty />
-            </Link>
-          }
-        />
-        <Route path={ROUTE.HOME} element={<DefaultLayout />}>
-          <Route index element={<Home />} />
-        </Route>
-      </Routes>
+    <div>
+      <Login />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,118 @@
+import React, { useState } from "react";
+import "../Login/Login.css";
+import {
+  AiFillFacebook,
+  AiFillGoogleCircle,
+  AiFillLinkedin,
+} from "react-icons/ai";
+const Login = () => {
+  const [isLogin, setIsLogin] = useState(false);
+  const loginButton = () => {
+    setIsLogin(!isLogin);
+
+    console.log("isLogin:", isLogin);
+  };
+  const registerButton = () => {
+    console.log("2:", 2);
+    setIsLogin(!isLogin);
+
+    // isLogin ? setIsLogin(false) : setIsLogin(true);
+  };
+
+  return (
+    <div>
+      <div
+        className={`container${isLogin ? "" : " right-panel-active"}`}
+        id="container"
+      >
+        <div className="form-container register-container">
+          <form action="#">
+            <h1>Register hire.</h1> <input type="text" placeholder="Name" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <button>Registerggggggg</button>
+            <span>or use your account</span>
+            <div className="social-container">
+              <a href="google.vn" className="social">
+                <i class="fa-brands fa-square-facebook"></i>
+              </a>
+              <a href="#" className="social">
+                <i className="lni lni-google" />
+              </a>
+
+              <a href="#" className="social">
+                <i className="lni lni-linkedin-original" />
+              </a>
+            </div>
+          </form>
+        </div>
+        <div className="form-container login-container">
+          <form action="#">
+            <h1>Login hire.</h1> <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <div className="content">
+              <div className="checkbox">
+                <input type="checkbox" name="checkbox" id="checkbox" />
+                <label>Remember me</label>
+              </div>
+              <div className="pass-link">
+                <a href="#">Forgot password?</a>
+              </div>
+            </div>
+            <button onClick={() => loginButton()}>Login1</button>
+            <span>or use your account</span>
+            <div className="social-container">
+              <a href="#" className="social">
+                <i className="lni lni-facebook-fill" />
+              </a>
+              <a href="#" className="social">
+                <i className="lni lni-google" />
+              </a>
+              <a href="#" className="social">
+                <i className="lni lni-linkedin-original" />
+              </a>
+            </div>
+          </form>
+        </div>
+        <div className="overlay-container">
+          <div className="overlay">
+            <div className="overlay-panel overlay-left">
+              <h1 className="title">
+                Hello <br /> friends
+              </h1>
+              <p>if Yout have an account, login here and have fun</p>
+              <button
+                className="ghost"
+                id="login"
+                onClick={() => {
+                  registerButton();
+                }}
+              >
+                Logineeeee
+                <i className="lni lni-arrow-left login" />
+              </button>
+            </div>
+            <div className="overlay-panel overlay-right">
+              <h1 className="title">
+                Start yout <br /> journy now
+              </h1>
+              <p>
+                if you don't have an account yet, join us and start your
+                journey.
+              </p>
+              <button
+                onClick={() => registerButton()}
+                className="ghost"
+                id="register"
+              >
+                Register <i className="lni lni-arrow-right register" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
